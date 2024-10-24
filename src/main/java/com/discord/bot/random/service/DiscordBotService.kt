f33703447e785bc8794b9237ed6ec5634fadcf4b
outmaneuver <72673084+outmaneuver@users.noreply.github.com>
2024-10-24 04:50:33 +0300

    Add rank retrieval feature
    Fixes #3
    
    Add a feature to retrieve the user's current rank using the discord-linked `riot_account_id`.
    
    * Add a new command to `DiscordBotService.kt` to retrieve rank information from the Riot API.
      * Use the endpoint `/lol/league/v4/entries/by-summoner/{encryptedSummonerId}` to fetch rank information.
      * Retrieve the `riot_account_id` from the user table and use it to call the Riot API.
      * Register the new command in the `setCommand` method.
    * Update `application.properties` to include configuration for the Riot API endpoint.

--- src/main/java/com/discord/bot/random/service/DiscordBotService.kt
+++ src/main/java/com/discord/bot/random/service/DiscordBotService.kt
@@ -9,6 +9,13 @@ import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 import org.springframework.stereotype.Service
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.client.RestTemplate
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 
 @Service
 class DiscordBotService : ListenerAdapter() {
@@ -16,6 +23,12 @@ class DiscordBotService : ListenerAdapter() {
     companion object {
         private const val ROLL_MESSAGE = "top: {0}\njg: {1}\nmid: {2}\nadc: {3}\nsup: {4}"
     }
+
+    @Value("\${riot.api.key}")
+    private lateinit var riotApiKey: String
+
+    @Value("\${riot.api.url}")
+    private lateinit var riotApiUrl: String
 
     /**
      * ボットにコマンドを追加する。
@@ -30,6 +43,10 @@ class DiscordBotService : ListenerAdapter() {
             .addOption(OptionType.STRING, "member4", "参加メンバー4")
             .addOption(OptionType.STRING, "member5", "参加メンバー5")
         jda.updateCommands().addCommands(testCommand).queue()
+
+        val rankCommand = Commands.slash("rank", "現在のランクを取得する")
+            .addOption(OptionType.STRING, "discord_id", "DiscordユーザーID")
+        jda.updateCommands().addCommands(rankCommand).queue()
     }
 
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
@@ -48,7 +65,64 @@ class DiscordBotService : ListenerAdapter() {
             val result = MessageFormat.format(ROLL_MESSAGE, suffledList[0], suffledList[1], suffledList[2],
                 suffledList[3], suffledList[4])
             event.reply(result).setEphemeral(false).queue()
+        } else if (command == "rank") {
+            val discordId = event.getOption("discord_id")?.asString
+            if (discordId != null) {
+                val riotAccountId = getRiotAccountId(discordId)
+                if (riotAccountId != null) {
+                    val rankInfo = getRankInfo(riotAccountId)
+                    if (rankInfo != null) {
+                        event.reply(rankInfo).setEphemeral(false).queue()
+                    } else {
+                        event.reply("ランク情報を取得できませんでした。").setEphemeral(true).queue()
+                    }
+                } else {
+                    event.reply("RiotアカウントIDを取得できませんでした。").setEphemeral(true).queue()
+                }
+            } else {
+                event.reply("DiscordユーザーIDが指定されていません。").setEphemeral(true).queue()
+            }
         }
     }
-}
 
+    private fun getRiotAccountId(discordId: String): String? {
+        // ユーザテーブルからdiscordIdに紐づいたriot_account_idを取得する処理を実装する
+        // ここでは仮に固定のriot_account_idを返す
+        return "sampleRiotAccountId"
+    }
+
+    private fun getRankInfo(riotAccountId: String): String? {
+        val restTemplate = RestTemplate()
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        headers.set("X-Riot-Token", riotApiKey)
+
+        val entity = HttpEntity<String>(headers)
+        val url = "$riotApiUrl/lol/league/v4/entries/by-summoner/$riotAccountId"
+
+        val response: ResponseEntity<Array<RiotRankInfo>> = restTemplate.exchange(url, HttpMethod.GET, entity, Array<RiotRankInfo>::class.java)
+        val rankInfo = response.body
+
+        return if (rankInfo != null && rankInfo.isNotEmpty()) {
+            rankInfo[0].toString()
+        } else {
+            null
+        }
+    }
+
+    data class RiotRankInfo(
+        val leagueId: String,
+        val queueType: String,
+        val tier: String,
+        val rank: String,
+        val summonerId: String,
+        val summonerName: String,
+        val leaguePoints: Int,
+        val wins: Int,
+        val losses: Int,
+        val veteran: Boolean,
+        val inactive: Boolean,
+        val freshBlood: Boolean,
+        val hotStreak: Boolean
+    )
+}
